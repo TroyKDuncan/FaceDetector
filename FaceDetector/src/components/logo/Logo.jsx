5f33a8dda03f24a1004bd1d5ea33d4fac4cb82c5
Troy Duncan <troyduncanbusiness@gmail.com>
2024-08-22 23:47:47 -0500

    Working on FaceDetector App

---
+++ FaceDetector/src/components/logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt'
+
+const Logo = () => {
+    return (
+        <Tilt className="grid place-content-center size-40 bg-white">
+            <div className="flex justify-center">
+                <h1 className="">This is Cool</h1>
+            </div>
+        </Tilt>
+    )
+}
+
+export default Logo;
---
+++ FaceDetector/src/components/navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav className="text-white absolute top-4 right-8 hover:text-slate-400 hover:underline hover:cursor-pointer">
+      <p>Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
