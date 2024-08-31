a0334fc2640206f5bdf8dfa8a581aeaf38bc3ce7
Troy Duncan <troyduncanbusiness@gmail.com>
2024-08-31 00:24:30 -0500

    I think the API is working with the new implementation, but I need to build out other features to see for sure

--- FaceDetector/src/App.jsx
+++ FaceDetector/src/App.jsx
@@ -5,18 +5,67 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/rank/Rank";
 import ParticlesBg from "particles-bg";
 
+const returnClarifaiRequestOptions = (imageURL) => {
+  const PAT = "34a199498cd84f50b50ab92b2ab6fbbb";
+  const USER_ID = "luvche";
+  const APP_ID = "my-app";
+  const MODEL_ID = "face-detection";
+  const IMAGE_URL = imageURL;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const onInputChange = (event) => {
     console.log(event.target.value);
-    setInput(event.target.value)
-    console.log(input)
+    setInput(event.target.value);
+    console.log(input);
   };
 
   const onButtonDetect = (event) => {
-    console.log('click');
+    console.log("click");
+
+    fetch(
+      "https://api.clarifai.com/v2/models/" +
+        "face-detection" +
+        "/versions/" +
+        "/outputs",
+      returnClarifaiRequestOptions(input)
+    )
+      .then((response) => response.json())
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {}
+      );
   };
 
   return (
