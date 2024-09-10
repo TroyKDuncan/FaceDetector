1bf75c487dad068f091bd3dacfc77d06f3f58791
TroyKDuncan <troyduncanbusiness@gmail.com>
2024-09-09 22:53:07 -0500

    Updated thank you page wording

--- FaceDetector/src/components/register/ThankYou.jsx
+++ FaceDetector/src/components/register/ThankYou.jsx
@@ -32,7 +32,7 @@ const ThankYou = ({ onRouteChange }) => {
       <div className="px-4 py-2 text-white w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-4 space-y-4 md:space-y-6 sm:p-8 flex flex-col items-center text-xl">
           <p>Thank you for signing up!</p>
-          <p>Go ahead and sign in next!</p>
+          <p>Here is the sign in page!</p>
         </div>
       </div>
     </div>
