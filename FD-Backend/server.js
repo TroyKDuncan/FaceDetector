64b12c41801106df1b0df5736e79f9f3bee3605c
Troy Duncan <troyduncanbusiness@gmail.com>
2024-09-23 12:01:37 -0500

    Working on the backend

--- FD-Backend/server.js
+++ FD-Backend/server.js
@@ -41,7 +41,7 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const { email, name, password } = req.body
+  const { email, name, password } = req.body;
   database.users.push({
     id: "125",
     name: name,
@@ -50,19 +50,36 @@ app.post("/register", (req, res) => {
     entries: 0,
     joined: new Date(),
   });
-  res.json(database.users[database.users.length-1])
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
 });
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
-
-/* 
-
-/                --> res = this is working
-/signin          --> POST = success/fail
-/register        --> POST = user
-/profile/:userId --> GET = user
-/image           --> PUT --> user
-
-*/
